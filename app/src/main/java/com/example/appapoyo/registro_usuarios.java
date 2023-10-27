17fbe673ac697f066febf72d1672cb1996a7ee13
armando.apaza <apazanacho@gmail.com>
2023-10-27 17:19:17 -0400

    agregacion de titulos en pantalla registro, y validaciones. so se puede crear los datos en la base a un

--- app/src/main/java/com/example/appapoyo/registro_usuarios.java
+++ app/src/main/java/com/example/appapoyo/registro_usuarios.java
@@ -10,15 +10,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class registro_usuarios extends AppCompatActivity {
 
-    private EditText firstNameEditText, lastNameEditText, emailEditText, passwordEditText, paraleloEditText;
+    private EditText firstNameEditText, lastNameEditText, emailEditText, passwordEditText, paraleloEditText, gestion;
+
+
     private RadioGroup userTypeRadioGroup;
     private RadioButton estudiante_boton,profesor_boton;
     private Button registerButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +31,13 @@ public class registro_usuarios extends AppCompatActivity {
 
         firstNameEditText = findViewById(R.id.firstName);
         lastNameEditText = findViewById(R.id.lastName);
+
         emailEditText = findViewById(R.id.email);
         passwordEditText = findViewById(R.id.password);
         paraleloEditText = findViewById(R.id.paralelo);
         estudiante_boton = findViewById(R.id.estudiante_boton);
         profesor_boton = findViewById(R.id.profesor_boton);
+        gestion = findViewById(R.id.gestion);
         registerButton = findViewById(R.id.registerButton);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +49,20 @@ public class registro_usuarios extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
                 String paralelo = paraleloEditText.getText().toString();
+                String gestionStr = gestion.getText().toString();
+                int gestion1 = 0;  // Valor por defecto en caso de error
+
+                try {
+                    gestion1 = Integer.parseInt(gestionStr);
+                    if (gestion1 < 2022 || gestion1 > 2030) {
+                        Toast.makeText(getApplicationContext(), "El valor de gestión no está dentro del rango permitido", Toast.LENGTH_SHORT).show();
+
+                        // Puedes agregar más lógica aquí, como limpiar el campo de entrada o realizar otras acciones.
+                    }
+                } catch (NumberFormatException e) {
+
+                    // Maneja la excepción si el valor ingresado no es un entero válido.
+                }
 
                 int selectedUserTypeId = userTypeRadioGroup.getCheckedRadioButtonId();
                 RadioButton selectedUserType = findViewById(selectedUserTypeId);
@@ -51,8 +71,14 @@ public class registro_usuarios extends AppCompatActivity {
                     String userType = selectedUserType.getText().toString();
                     // Aquí puedes realizar la lógica de registro con los datos ingresados.
                 }
+
             }
         });
+
+
+
+
+
 
     }
 }
